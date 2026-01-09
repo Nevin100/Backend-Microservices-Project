b1abd8bceca6b7e30fb40b50f1fe68ac4420ca55
Nevin100 <opopopopop100000@gmail.com>
2026-01-09 13:36:24 +0530

    Started with 1st service -> User Service

---
+++ user-service/src/Utils/Db.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose"
+
+const DB = async() =>{
+    try {
+        await mongoose.connect(process.env.MONGODB_URL)
+    console.log("Successfully estd connection with MONGODB Database")
+    } catch (error) {
+        console.log(error);
+        process.exit(1);    
+    }
+}
+
+export default DB;
